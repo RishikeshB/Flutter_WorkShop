bd876e4e133e50dafc7ac33a9d7c633990525638
Rishikesh_B <rishikesh@theproindia.com>
2024-09-02 07:46:09 +0530

    feat: basics of generics and wigets and objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-      home: Scaffold(body: Center(child: Text("Hello World")))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.red],
+            ),
+          ),
+          child: const Center(
+            child: Text("Hello World"),
+          ),
+        ),
+      ),
+    ),
+  );
 }
